1f2c762ccb6e4a6b719e4cc5b5cf77f37fa14639
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-22 16:31:01 -0700

    Refactor server code

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 #!/usr/bin/env node
-var net = require('net')
-var meta = require('./package.json')
-
-var LEVELDB = ( process.env.LEVELDB || ( '.' + meta.name + '.leveldb' ) )
-var level = require('level')(LEVELDB)
-var logs =  require('level-logs')(level, { valueEncoding: 'json' })
-var pino = require('pino')()
-var BLOBS = ( process.env.BLOBS || ( '.' + meta.name + '.blobs' ) )
-var blobs = require('fs-blob-store')(BLOBS)
-
-var emitter = new (require('events').EventEmitter)()
-
-var handler = require('./')(pino, logs, blobs, emitter)
-
-var server = net.createServer()
-
-server.on('connection', handler)
-
+var NAME = require('./package.json').name
+var LEVELDB = ( process.env.LEVELDB || ( '.' + NAME + '.leveldb' ) )
+var BLOBS = ( process.env.BLOBS || ( '.' + NAME + '.blobs' ) )
 var PORT = ( parseInt(process.env.PORT) || 8089 )
+
+var level = require('levelup')(LEVELDB, { db: require('leveldown') })
+var pino = require('pino')()
+var handler = require('./')(
+  pino,
+  require('level-logs')(level, { valueEncoding: 'json' }),
+  require('fs-blob-store')(BLOBS),
+  new (require('events').EventEmitter)())
+
+var server = require('net').createServer(handler)
+
 server.listen(PORT, function() {
   pino.info({ event: 'listening', port: this.address().port }) })
 
