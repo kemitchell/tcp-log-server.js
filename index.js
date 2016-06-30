ac60abee83783ee4fbbdea33bfd4bb78afdcbf71
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-30 09:55:42 -0700

    Use standard style

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var sha256 = require('sha256')
 var stringify = require('json-stable-stringify')
 var uuid = require('uuid')
 
-module.exports = function(serverLog, logs, blobs, emitter) {
-  return function(connection) {
+module.exports = function (serverLog, logs, blobs, emitter) {
+  return function (connection) {
     // Child log for this connection.
-    serverLog = serverLog.child({ connection: uuid.v4() })
-    serverLog.info({ event: 'connected' })
+    serverLog = serverLog.child({connection: uuid.v4()})
+    serverLog.info({event: 'connected'})
     connection
-      .on('end', function() { serverLog.info({ event: 'end' }) })
-      .on('close', function(error) { serverLog.info({ event: 'end', error: error }) })
+      .on('end', function () { serverLog.info({event: 'end'}) })
+      .on('close', function (error) { serverLog.info({event: 'end', error: error}) })
 
     // Send JSON back and forth across the connection.
     var json = duplexJSON(connection)
@@ -43,151 +43,180 @@ module.exports = function(serverLog, logs, blobs, emitter) {
     // An asynchronous queue for appending hashes to logs. Ensures that
     // each append operation can read the head of the log and number
     // itself appropriately.
-    var entriesQueue = queue(function(task, done) {
+    var entriesQueue = queue(function (task, done) {
       var log = task.log
       var hash = task.hash
-      serverLog.info({ event: 'appending', log: log, hash: hash })
-      logs.append(log, hash, done) })
+      serverLog.info({event: 'appending', log: log, hash: hash})
+      logs.append(log, hash, done)
+    })
 
-    json.on('data', function(message) {
-      serverLog.info({ event: 'message', message: message })
-      if (readMessage(message) && !reading) { onReadMessage(message) }
-      else if(writeMessage(message)) { onWriteMessage(message) } })
+    json.on('data', function (message) {
+      serverLog.info({event: 'message', message: message})
+      if (readMessage(message) && !reading) onReadMessage(message)
+      else if (writeMessage(message)) onWriteMessage(message)
+    })
 
-    function onReadMessage(message) {
+    function onReadMessage (message) {
       var log = message.log
-      reading = { doneStreaming: false, buffer: [ ], from: message.from }
+      reading = {doneStreaming: false, buffer: [], from: message.from}
 
       // New entries are emitted as they are made.
-      var onAppend = function(index, entry) {
+      var onAppend = function (index, entry) {
         // Do not send entries from earlier in the log than requested.
         if (reading.from <= index) {
           // Phase 3:
           if (reading.doneStreaming) {
-            serverLog.info({ event: 'forward', log: log, index: index })
-            sendEntry(log, index, entry) }
+            serverLog.info({event: 'forward', log: log, index: index})
+            sendEntry(log, index, entry)
           // Waiting for Phase 2
-          else {
-            serverLog.info({ event: 'buffer', log: log, index: index })
-            reading.buffer.push({ index: index, entry: entry }) } } }
+          } else {
+            serverLog.info({event: 'buffer', log: log, index: index})
+            reading.buffer.push({index: index, entry: entry})
+          }
+        }
+      }
       emitter.addListener(log, onAppend)
 
       // Phase 1: Stream index-hash pairs from the LevelUP store.
-      var streamLog = serverLog.child({ phase: 'stream', log: log })
-      streamLog.info({ event: 'create' })
-      var streamOptions = { since: message.from }
+      var streamLog = serverLog.child({phase: 'stream', log: log})
+      streamLog.info({event: 'create'})
+      var streamOptions = {since: message.from}
       var stream = logs.createReadStream(log, streamOptions)
       reading.stream = stream
       stream
-        .on('data', function(data) {
-          var chunks = [ ]
+        .on('data', function (data) {
+          var chunks = []
           var errored = false
           // Use the hash from LevelUP to look up the message data in
           // the blob store.
-          blobs.createReadStream({ key: hashToPath(data.value) })
-            .on('data', function(chunk) { chunks.push(chunk) })
-            .on('error', function(error) {
+          blobs.createReadStream({key: hashToPath(data.value)})
+            .on('data', function (chunk) { chunks.push(chunk) })
+            .on('error', function (error) {
               errored = true
               serverLog.error(error)
-              json.write(
-                { replyTo: message.id,
-                  log: message.log,
-                  blob: data.value,
-                  error: error.toString() }) })
-            .on('end', function() {
+              json.write({
+                replyTo: message.id,
+                log: message.log,
+                blob: data.value,
+                error: error.toString()
+              })
+            })
+            .on('end', function () {
               if (!errored) {
                 var value = JSON.parse(Buffer.concat(chunks))
-                sendEntry(log, data.seq, value) } }) })
-        .once('error', function(error) {
+                sendEntry(log, data.seq, value)
+              }
+            })
+        })
+        .once('error', function (error) {
           streamLog.error(error)
-          sendEntry(log, -1, { error: error.toString() }, true)
+          sendEntry(log, -1, {error: error.toString()}, true)
           emitter.removeListener(log, onAppend)
-          reading = false })
-        .once('end', function() {
-          streamLog.info({ event: 'end' })
+          reading = false
+        })
+        .once('end', function () {
+          streamLog.info({event: 'end'})
           // Phase 2: Entries may have been written while we were
           // streaming from LevelUP. Send them now.
-          reading.buffer.forEach(function(message) {
-            sendEntry(log, message.index, message.key, message.entry) })
+          reading.buffer.forEach(function (message) {
+            sendEntry(log, message.index, message.key, message.entry)
+          })
           // Mark the stream done so messages sent via the
           // EventEmitter will be written out to the socket, rather
           // than buffered.
           reading.buffer = null
-          reading.doneStreaming = true }) }
+          reading.doneStreaming = true
+        })
+    }
 
-    function onWriteMessage(message) {
+    function onWriteMessage (message) {
       var log = message.log
       var hash = sha256(stringify(message.entry))
-      var writeLog = serverLog.child({ hash: hash, log: log })
+      var writeLog = serverLog.child({hash: hash, log: log})
       // Append the entry payload in the blob store, by hash.
-      blobs.createWriteStream({ key: hashToPath(hash) })
-        .on('error', function(error) {
+      blobs.createWriteStream({key: hashToPath(hash)})
+        .on('error', function (error) {
           writeLog.error(error)
-          json.write(
-            { replyTo: message.id,
-              log: message.log,
-              error: error.toString() }) })
-        .on('finish', function() {
+          json.write({
+            replyTo: message.id,
+            log: message.log,
+            error: error.toString()
+          })
+        })
+        .on('finish', function () {
           // Append an entry in the LevelUP log with the hash of the payload.
-          entriesQueue.push({ log: log, hash: hash }, function(error, index) {
+          entriesQueue.push({log: log, hash: hash}, function (error, index) {
             if (error) {
               writeLog.error(error)
-              json.write(
-                { replyTo: message.id,
-                  log: message.log,
-                  error: error.toString() }) }
-            else {
+              json.write({
+                replyTo: message.id,
+                log: message.log,
+                error: error.toString()
+              })
+            } else {
               writeLog.info({ event: 'wrote' })
-              json.write(
-                { replyTo: message.id,
-                  log: message.log,
-                  event: 'wrote' })
+              json.write({
+                replyTo: message.id,
+                log: message.log,
+                event: 'wrote'
+              })
               // Emit an event.
-              emitter.emit(log, index, message.entry) } }) })
-        .end(stringify(message.entry), 'utf8') }
+              emitter.emit(log, index, message.entry)
+            }
+          })
+        })
+        .end(stringify(message.entry), 'utf8')
+    }
 
-    function sendEntry(log, index, entry, end) {
-      json[end ? 'end' : 'write']({ log: log, index: index, entry: entry })
-      serverLog.info({ event: 'sent', log: log, index: index }) } } }
+    function sendEntry (log, index, entry, end) {
+      json[end ? 'end' : 'write']({log: log, index: index, entry: entry})
+      serverLog.info({event: 'sent', log: log, index: index})
+    }
+  }
+}
 
-function readMessage(argument) {
+function readMessage (argument) {
   return (
     isMessage(argument) &&
     has(argument, 'type', 'read') &&
     has(argument, 'from', isPositiveInteger) &&
-    has(argument, 'log', isString) ) }
+    has(argument, 'log', isString)
+  )
+}
 
-function writeMessage(argument) {
+function writeMessage (argument) {
   return (
     isMessage(argument) &&
     has(argument, 'type', 'write') &&
     has(argument, 'log', isString) &&
-    has(argument, 'entry', function(entry) {
-      return ( typeof entry === 'object' ) }) ) }
+    has(argument, 'entry', function (entry) {
+      return typeof entry === 'object'
+    })
+  )
+}
 
-function isMessage(argument) {
+function isMessage (argument) {
   return (
-    ( typeof argument === 'object' ) &&
-    has(argument, 'id', function(id) {
-      return ( id.length > 0 ) }) ) }
+    typeof argument === 'object' &&
+    has(argument, 'id', function (id) { return id.length > 0 })
+  )
+}
 
-function has(argument, key, predicate) {
-  return (
-    argument.hasOwnProperty(key) &&
-    ( ( typeof predicate === 'function' )
-        ? predicate(argument[key])
-        : ( argument[key] === predicate ) ) ) }
+function has (argument, key, predicate) {
+  return argument.hasOwnProperty(key) &&
+    typeof predicate === 'function'
+      ? predicate(argument[key])
+      : argument[key] === predicate
+}
 
-function isPositiveInteger(argument) {
-  return (
-    ( typeof argument === 'number' ) &&
-    ( argument % 1 === 0 ) &&
-    ( argument >= 0 ) ) }
+function isPositiveInteger (argument) {
+  return Number.isInteger(argument) && argument >= 0
+}
 
-function isString(argument) {
-  return (
-    ( typeof argument === 'string' ) &&
-    ( argument.length > 0 ) ) }
+function isString (argument) {
+  return typeof argument === 'string' && argument.length > 0
+}
 
-function hashToPath(hash) {
-  return ( hash.slice(0, 2) + '/' + hash.slice(2) ) }
+function hashToPath (hash) {
+  return hash.slice(0, 2) + '/' + hash.slice(2)
+}
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 var NAME = require('./package.json').name
-var LEVELDB = ( process.env.LEVELDB || ( '.' + NAME + '.leveldb' ) )
-var BLOBS = ( process.env.BLOBS || ( '.' + NAME + '.blobs' ) )
-var PORT = ( parseInt(process.env.PORT) || 8089 )
+var LEVELDB = process.env.LEVELDB || ('.' + NAME + '.leveldb')
+var BLOBS = process.env.BLOBS || ('.' + NAME + '.blobs')
+var PORT = parseInt(process.env.PORT) || 8089
 
-var level = require('levelup')(LEVELDB, { db: require('leveldown') })
+var level = require('levelup')(LEVELDB, {db: require('leveldown')})
 var pino = require('pino')()
 var handler = require('./')(
   pino,
-  require('level-logs')(level, { valueEncoding: 'json' }),
+  require('level-logs')(level, {valueEncoding: 'json'}),
   require('fs-blob-store')(BLOBS),
-  new (require('events').EventEmitter)())
+  new (require('events').EventEmitter)()
+)
 
 var server = require('net').createServer()
   .on('connection', handler)
-  .on('close', function() { level.close() })
+  .on('close', function () { level.close() })
 
-server.listen(PORT, function() {
-  pino.info({ event: 'listening', port: this.address().port }) })
+server.listen(PORT, function () {
+  pino.info({event: 'listening', port: this.address().port})
+})
 
-process.on('exit', function() { server.close() })
+process.on('exit', function () { server.close() })
